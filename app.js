fd62eed36014b941ab44bbf6ab178dff7e296ff0
EpicMaDneSss <120367585+EpicMaDneSss@users.noreply.github.com>
2023-03-30 22:03:21 +0200

    orthographe des commentaire

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-document.body.onload = function () {// fonction qui se declare au chargement de la page et qui prend les 2 variables ci-dessous
+document.body.onload = function () {// fonction qui se déclare au chargement de la page et qui prend les 2 variables ci-dessous
     nbr = 8; // La variable "nbr" est définie sur 8, ce qui signifie que le carrousel contiendra 8 éléments
     p = 0; //La variable "p" est initialisée à 0, ce qui signifie que le carrousel commencera au début
-    container = document.getElementById('container');// on recupere le conteneur en utilisant son ID
-    g = document.querySelector('.fa-chevron-left');//on utilise la methode querySelector pour recuperer les 2 icones et les stocker
+    container = document.getElementById('container');// on récupère le conteneur en utilisant son ID
+    g = document.querySelector('.fa-chevron-left');//on utilise la méthode querySelector pour récupérer les 2 icônes et les stocker
     d = document.querySelector('.fa-chevron-right');
-    container.style.width = (190 * nbr) + "px"; // on defini la largeur du conteneur avec la propriété style.width en multipliant la taille de l'img (190px) au nombre d'elements (8)
+    container.style.width = (190 * nbr) + "px"; // on définit la largeur du conteneur avec la propriété style.width en multipliant la taille de l'img (190px) au nombre d'éléments (8)
 }
 
-g.onclick = function () { // cette fonctione est liée a l'icone gauche
-    if (p > -nbr + 1)//      on verifie si la position du carrousel est superieur a la valeur max autorisé (le nbr d'element du carrousel)
-        p--;//               si oui diminue la position de 1 (donc ce deplace vers la gauche)
+g.onclick = function () { // cette fonction est liée à l'icône gauche
+    if (p > -nbr + 1)//      on vérifie si la position du carrousel est supérieure à la valeur max autorisée (le nbr d'éléments du carrousel)
+        p--;//               si oui diminue la position de 1 (donc se déplace vers la gauche)
     else
-        p = 0;//             si ça valeur et egale à 0 on met à jour la position en utilisant la proprieté transform  (qui a pour effet donner une impression de carrousel infini)
+        p = 0;//             si sa valeur est egale à 0 on met à jour la position en utilisant la proprieté transform  (qui a pour effet donner une impression de carrousel infini)
     container.style.transform = 'translate(' + p * 190 + 'px)';
     container.style.transition = "all 1s ease";// la propriété translate permet d'ajouter une animation plus fluide
 }
 
-d.onclick = function () {// cette fonctione est liée a l'icone droit
-    if (p < 0)//            on verifie si la position du carrousel est inferieurà 0 si oui ou incremente(augmente) la position de 1
+d.onclick = function () {// cette fonction est liée à l'icône droit
+    if (p < 0)//            on vérifie si la position du carrousel est inférieure à 0 si oui on incrémente(augmente) la position de 1
         p++;
     else//                  sinon elle réinitialise la position à la valeur maximale autorisée
         p = -nbr + 1;
@@ -27,22 +27,22 @@ d.onclick = function () {// cette fonctione est liée a l'icone droit
 //////////////////////////////////////////////////////////////////
 
 // Créer la fenêtre modale
-const modal = document.createElement('div');// on crée unu div qui a pour constante modal
+const modal = document.createElement('div');// on crée une div qui a pour constante modal
 modal.classList.add('modal');//on lui donne une classe .modal
-modal.style.display = 'none';// on lui mais la proprieté display none pour masquer la fenetre
+modal.style.display = 'none';// on lui met la proprieté display none pour masquer la fenêtre
 
-// on Créer l'image et la description grace a la methode "createElement"
+// on Créé l'image et la description grâce à la méthode "createElement"
 const image = document.createElement('img');
 const description = document.createElement('p');
 
-// on ajoute l'image et la description à la fenêtre modale grace a la methode "appenchild"
+// on ajoute l'image et la description à la fenêtre modal grâce à la méthode "appenchild"  qui permet d'ajouter un nouvel élément enfant à un élément parent existant.
 modal.appendChild(image);
 modal.appendChild(description);
 
-// Ajouter la fenêtre modale à la page
+// Ajouter la fenêtre modal à la page
 document.body.appendChild(modal);
 
-// on ajoute un événement de clic pour fermer la fenêtre modale lorsque l'utilisateur click dessus grace a la methode "addEventListener"
+// on ajoute un évènement de clic pour fermer la fenêtre modal lorsque l'utilisateur click dessus grace a la methode "addEventListener"
 modal.addEventListener('click', () => {
   modal.style.display = 'none';
 });
